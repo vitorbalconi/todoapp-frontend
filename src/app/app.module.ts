c90abcc88d2ee67f37c54a0a17c9d7794dba9d8a
vitorbalconi <vitor@petize.com.br>
2023-09-28 02:04:25 -0300

    refactor: change the layout and finish todo page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './pages/login/login.component';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TodoListComponent
+    TodoListComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -0,0 +1,135 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable, combineLatest, concatMap, debounceTime, filter, forkJoin, merge, mergeAll, startWith, take, zip } from 'rxjs';
+import { TodoRepositoryService } from 'src/app/domain/data/todo-repository.service';
+import { Priority, PriorityUtil } from 'src/app/domain/enums/priority.enum';
+import { TodoItemI } from 'src/app/domain/interfaces/todo.interface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent {
+
+  public date = new Date().toISOString().slice(0, 10)
+
+  public todoForm = new FormGroup( {
+    description: new FormControl('', Validators.required),
+    dueDate: new FormControl(this.date, Validators.required),
+    priority: new FormControl(PriorityUtil.getKey(Priority.LOW), Validators.required),
+    done: new FormControl(false, Validators.required)
+  })
+
+  public toDoList: Array<TodoItemI> = [];
+
+  public priorities = PriorityUtil.getIterableArray();
+
+  public priorityControl = new FormControl('');
+
+  public searchControl = new FormControl('');
+
+  public itensPerPageOptions = [5,10,20];
+
+  public itensPerPage = new FormControl(this.itensPerPageOptions[0]);
+
+  public currentPage = 1;
+
+  public lastPage = 1;
+
+  public firstPage = 1;
+
+  constructor(private toDoService: TodoRepositoryService) {
+    this.searchToDos();
+  }
+
+  public get disableNextPage(): boolean {
+    return this.currentPage >= this.lastPage;
+  }
+
+  public get disablePreviousPage(): boolean {
+    return this.currentPage <= this.firstPage;
+  }
+
+  public searchToDos(): void {
+    const timeToWaitUserInputs = 500;
+
+    combineLatest([
+      this.priorityControl.valueChanges.pipe(startWith('')),
+      this.searchControl.valueChanges.pipe(startWith('')),
+      this.itensPerPage.valueChanges.pipe(startWith(this.itensPerPageOptions[0]))
+    ])
+    .pipe(
+      debounceTime(timeToWaitUserInputs),
+      takeUntilDestroyed()
+    )
+    .subscribe(() => {
+      this.getToDos();
+    })
+  }
+
+  public getToDos(): void {
+    const priority = this.priorityControl.value ?? '';
+    const search = this.searchControl.value ?? '';
+    const itensPerPage = this.itensPerPage.value!
+
+    this.toDoService.getToDos(this.currentPage, itensPerPage, priority, search)
+    .pipe(take(1))
+    .subscribe(data => {
+      this.firstPage = data.firstPage;
+      this.lastPage = data.lastPage;
+      this.toDoList = data.toDos;
+    })
+  }
+
+  public addTodo(): void {
+    const newToDo = this.todoForm.value;
+
+    const newTodoNonNullish: Omit<TodoItemI, 'id'> = {
+      description: newToDo.description!,
+      done: false,
+      dueDate: newToDo.dueDate!,
+      priority: newToDo.priority!
+    }
+
+    this.toDoService.addToDo(newTodoNonNullish)
+    .pipe(take(1))
+    .subscribe(() => {
+      window.alert('Task created!');
+      this.todoForm.controls.description.setValue('');
+      this.getToDos()
+    })
+  }
+
+  public removeTodo(toDo: TodoItemI): void {
+
+    this.toDoService.deleteTask(toDo)
+    .pipe(take(1))
+    .subscribe(() => {
+      window.alert('Task deleted!');
+
+      this.getToDos();
+    })
+  }
+
+  public doUndoTask(todo: TodoItemI): void {
+    this.toDoService.doUndoTask(todo)
+    .pipe(take(1))
+    .subscribe(() => {
+      this.getToDos();
+    })
+  }
+
+  public nextPage(): void {
+    this.currentPage += 1;
+
+    this.getToDos();
+  }
+
+  public previousPage(): void {
+    this.currentPage -= 1;
+
+    this.getToDos();
+  }
+}
